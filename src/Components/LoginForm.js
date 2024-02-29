186af2496b3f742108507674d1049947b8506f43
Alaudhin936 <160020112+Alaudhin936@users.noreply.github.com>
2024-02-29 21:34:19 +0530

    Update LoginForm.js

--- src/Components/LoginForm.js
+++ src/Components/LoginForm.js
@@ -66,7 +66,7 @@ export default function LoginForm() {
             </div>
           </div>
           <div className="submit-btn">
-            <button style={{ color: "white" }}>
+            <button onClick={functiononlogin} style={{ color: "white" }}>
               Login
             </button>
           </div>
